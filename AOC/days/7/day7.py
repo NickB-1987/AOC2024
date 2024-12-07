d4c5d95f2277e3cd3139b4344cb1fcddc7469604
NickB-1987 <94359987+NickB-1987@users.noreply.github.com>
2024-12-07 11:11:35 +0000

    day 7 complete

--- AOC/days/7/day7.py
+++ AOC/days/7/day7.py
@@ -3,10 +3,40 @@ from pathlib import Path
 day = 7
 test = 0
 
-data = [
-    list(line.strip())
+data = [[int(line[0]), [int(num) for num in line[1].strip().split(" ")]] for line in [
+    line.strip().split(":")
     for line in open(Path(Path(__file__).parent, f"day{day}input"), "r").readlines()
-]
+]]
+
 
 if __name__ == "__main__":
-    ...
+    total = 0
+    vals = []
+    for ans, _nums in data:
+        nums = _nums.copy()
+        vals = []
+        vals.append(nums.pop(0))
+        while len(nums) > 0:
+            modifier = nums.pop(0)
+            vals = [val * modifier for val in vals] + [val + modifier for val in vals]
+        if ans in vals:
+            total += ans
+    print("Part 1: ",total)
+
+    # Part 2
+    vals = []
+    total = 0
+    for ans, _nums in data:
+        nums = _nums.copy()
+        vals = []
+        vals.append(nums.pop(0))
+        while len(nums) > 0:
+            modifier = nums.pop(0)
+            vals = [val * modifier for val in vals] + [val + modifier for val in vals] + [int(f"{val}{modifier}") for val in vals]
+        if ans in vals:
+            total += ans
+    print("Part 2: ",total)
+            
+
+
+
