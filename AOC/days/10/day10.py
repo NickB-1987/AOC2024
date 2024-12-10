772f1da452296ea4dddaeb2c7d5ed185e5b19c9d
NickB-1987 <94359987+NickB-1987@users.noreply.github.com>
2024-12-10 07:51:19 +0000

    started d10

--- AOC/days/10/day10.py
+++ AOC/days/10/day10.py
@@ -1,10 +1,50 @@
 from pathlib import Path
+from queue import Queue
 
 day = 10
 test = 0
 
-data = open(Path(Path(__file__).parent, f"day{day}input"), "r").readline().strip()
+data = [[int(x) for x in list(line.strip())] for line in open(Path(Path(__file__).parent, f"day{day}test"), "r").readlines()]
+
+maxy = len(data) - 1
+maxx = len(data[0]) - 1
+
+coords = set()
+
+def coord_oob(coord, direction) -> bool:
+    if ((coord[0] + direction[0]) > maxx or
+        (coord[0] + direction[0]) < 0 or
+        (coord[1] + direction[1]) > maxy or
+        (coord[1] + direction[1]) < 0):
+        return True
+    else:
+        return False
+
+def move(coord, direction) -> tuple[int]:
+    return ((coord[0] + direction[0], coord[1] + direction[1]))
 
 
 if __name__ == "__main__":
-    ...
+    for y, line in enumerate(data):
+        for x, val in enumerate(line):
+            if val == 0:
+                coords.add((x, y))
+    i = 1
+    while i < 10:
+        new_coords = set()
+        :
+            coord = coords.pop()
+            for direction in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                if not coord_oob(coord, direction):
+                    new = move(coord, direction)
+                    if data[new[1]][new[0]] == i:
+                        new_coords.add(new)
+        coords = new_coords
+        i += 1
+        print(len(new_coords))
+
+
+
+
+            
+
--- scratch.py
+++
@@ -1,3 +0,0 @@
-idx = 6
-character = 5
-print(str(int(idx / 2)) * int(character))
