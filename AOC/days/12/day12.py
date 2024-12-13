1de8df032585d09291d7b9ad1f1355a7c4c225b6
NickB-1987 <94359987+NickB-1987@users.noreply.github.com>
2024-12-13 14:37:37 +0000

    day 12 finished

--- AOC/days/12/day12.py
+++ AOC/days/12/day12.py
@@ -31,28 +31,58 @@ def find_region(letter: str, coord: tuple[int]):
     q = Queue()
     q.put(coord)
     edges = 0
+    sides = 0
     while not q.empty():
         square = q.get()
-        for direction in directions:
-            if is_neighbour(letter, square, direction):
-                new = (square[0] + direction[0], square[1] + direction[1])
+        for dr, dc in directions:
+            if is_neighbour(letter, square, (dr, dc)):
+                new = (square[0] + dr, square[1] + dc)
                 if new not in squares:
                     q.put(new)
                 squares.add(new)
             else:
                 edges += 1
-    return squares, edges
+    for square in squares:
+        # Check top left
+        tl = is_neighbour(letter, square, (-1, -1))
+        tc = is_neighbour(letter, square, (0, -1))
+        tr = is_neighbour(letter, square, (1, -1))
+        ml = is_neighbour(letter, square, (-1, 0))
+        mr = is_neighbour(letter, square, (1, 0))
+        bl = is_neighbour(letter, square, (-1, 1))
+        bc = is_neighbour(letter, square, (0, 1))
+        br = is_neighbour(letter, square, (1, 1))
+        if (not tl) and tc and ml:
+            sides += 1
+        if tc and (not tr) and mr:
+            sides += 1
+        if mr and (not br) and bc:
+            sides += 1
+        if bc and (not bl) and ml:
+            sides += 1
+        if (not tc) and (not ml):
+            sides += 1
+        if (not tc) and (not mr):
+            sides += 1
+        if (not bc) and (not mr):
+            sides += 1
+        if (not bc) and (not ml):
+            sides += 1
+
+    return squares, edges, sides
 
 
 if __name__ == "__main__":
     price = 0
+    second_price = 0
     already_found = set()
     for y in range(maxy + 1):
         for x in range(maxx + 1):
             if (x, y) not in already_found:
                 letter = data[y][x]
-                region, edges = find_region(letter, (x, y))
+                region, edges, sides = find_region(letter, (x, y))
                 already_found = already_found.union(region)
-                this_price = len(region) * edges
-                price += this_price
-    print(price)
+                price += len(region) * edges
+                second_price += len(region) * sides
+    print("Part1: ", price)
+    print("Part2: ", price)
