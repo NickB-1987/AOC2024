8fb7b0f360e84c31196df86456e944996257d8e0
NickB-1987 <94359987+NickB-1987@users.noreply.github.com>
2024-12-16 14:02:27 +0000

    d16 pt1 complete

--- AOC/days/16/day16.py
+++ AOC/days/16/day16.py
@@ -1,11 +1,11 @@
 from pathlib import Path
-from heapq import heappush, heappop
+from queue import PriorityQueue
 
 day = 16
 
 data = [
     list(line.strip())
-    for line in open(Path(Path(__file__).parent, f"day{day}test2"), "r").readlines()
+    for line in open(Path(Path(__file__).parent, f"day{day}input"), "r").readlines()
 ]
 
 directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
@@ -16,41 +16,47 @@ for y, line in enumerate(data):
     for x, char in enumerate(line):
         if char == "S":
             start = (x, y)
+            grid[(x, y)] = "."
         elif char == "E":
             end = (x, y)
+            grid[(x, y)] = "."
         else:
             grid[(x, y)] = char
 
+
 def turn_left(d):
     return (d - 1) % 4
+
+
 def turn_right(d):
     return (d + 1) % 4
 
-q = []
+
+q = PriorityQueue()
 
 # store steps as (score, (x, y), direction)
 # forward = 1 point
 # right or left = 1000 points
-
-heappush(q, (0, start, 1))
+q.put((0, start, 1))
 
 memo = {}
 
 if __name__ == "__main__":
-    while q:
-        # print(len(q))
-        score, pos, direction = heappop(q)
+    while q.not_empty:
+        score, pos, direction = q.get()
         if pos in memo and memo[pos][0] == direction and memo[pos][1] <= score:
             continue
+        memo[pos] = (direction, score)
         if pos == end:
             print(score)
             break
 
-        right = (score + 1000, pos, turn_right(direction))
-        left = (score + 1000, pos, turn_left(direction))
-        forward = (score + 1, (pos[0] + directions[direction][0], pos[1] + directions[direction][1]), direction)
-        if grid.get(forward[1], None) != "#":
-            heappush(q, forward)
-        heappush(q, right)
-        heappush(q, left)
-
+        q.put((score + 1000, pos, turn_right(direction)))
+        q.put((score + 1000, pos, turn_left(direction)))
+        forward = (
+            score + 1,
+            (pos[0] + directions[direction][0], pos[1] + directions[direction][1]),
+            direction,
+        )
+        if grid[forward[1]] != "#":
+            q.put(forward)
