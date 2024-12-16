0137554553f792d733b907a48db53e7cb662fa4e
NickB-1987 <94359987+NickB-1987@users.noreply.github.com>
2024-12-16 14:38:04 +0000

    try d16 pt2

--- AOC/days/16/day16.py
+++ AOC/days/16/day16.py
@@ -5,7 +5,7 @@ day = 16
 
 data = [
     list(line.strip())
-    for line in open(Path(Path(__file__).parent, f"day{day}input"), "r").readlines()
+    for line in open(Path(Path(__file__).parent, f"day{day}test2"), "r").readlines()
 ]
 
 directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
@@ -37,26 +37,51 @@ q = PriorityQueue()
 # store steps as (score, (x, y), direction)
 # forward = 1 point
 # right or left = 1000 points
-q.put((0, start, 1))
+q.put((0, start, 1, set()))
 
 memo = {}
 
 if __name__ == "__main__":
-    while q.not_empty:
-        score, pos, direction = q.get()
-        if pos in memo and memo[pos][0] == direction and memo[pos][1] <= score:
-            continue
-        memo[pos] = (direction, score)
-        if pos == end:
-            print(score)
-            break
+    results = []
+    target_score = 0
+    
+    finish = False
 
-        q.put((score + 1000, pos, turn_right(direction)))
-        q.put((score + 1000, pos, turn_left(direction)))
+    while q.not_empty and not finish:
+        score, pos, direction, path = q.get()
+        # if pos in memo and memo[pos][0] == direction and memo[pos][1] <= score:
+        #     continue
+        # memo[pos] = (direction, score)
+        
+        if pos == end:
+            if not results:
+                target_score = score
+                print(score)
+                results.append(path)
+            if score == target_score:
+                results.append(path)
+            if score > target_score:
+                finish = True
+
+        path.add(pos)
+
+        q.put((score + 1000, pos, turn_right(direction), path.copy()))
+        q.put((score + 1000, pos, turn_left(direction), path.copy()))
         forward = (
             score + 1,
             (pos[0] + directions[direction][0], pos[1] + directions[direction][1]),
-            direction,
+            direction, path.copy()
         )
         if grid[forward[1]] != "#":
             q.put(forward)
+    myset = set()
+    for i in results:
+        myset = myset.union(i)
+    for y, line in enumerate(data):
+        for x, char in enumerate(line):
+            if (x, y) in myset:
+                print("O", end="")
+            else:
+                print(char, end="")
+        print()
+    print(len(myset))
