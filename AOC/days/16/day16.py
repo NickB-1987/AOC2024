a68ebf7ec826c468b33a53a06d83db4047e257df
NickB-1987 <94359987+NickB-1987@users.noreply.github.com>
2024-12-17 09:07:23 +0000

    deconflict

--- AOC/days/16/day16.py
+++ AOC/days/16/day16.py
@@ -1,17 +1,17 @@
 from pathlib import Path
 from queue import PriorityQueue
-
+ 
 day = 16
-
+ 
 data = [
     list(line.strip())
-    for line in open(Path(Path(__file__).parent, f"day{day}test2"), "r").readlines()
+    for line in open(Path(Path(__file__).parent, f"day{day}input"), "r").readlines()
 ]
-
+ 
 directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-
+ 
 grid: dict[tuple[int, int], str] = {}
-
+ 
 for y, line in enumerate(data):
     for x, char in enumerate(line):
         if char == "S":
@@ -22,67 +22,41 @@ for y, line in enumerate(data):
             grid[(x, y)] = "."
         else:
             grid[(x, y)] = char
-
-
-def turn_left(d):
-    return (d - 1) % 4
-
-
-def turn_right(d):
-    return (d + 1) % 4
-
-
+ 
 q = PriorityQueue()
-
-# store steps as (score, (x, y), direction)
-# forward = 1 point
-# right or left = 1000 points
-q.put((0, start, 1, set()))
-
-memo = {}
-
+ 
+q.put((0, start, 1, [start]))
+ 
+memo = set()
+ 
 if __name__ == "__main__":
-    results = []
-    target_score = 0
-
+    results = set()
+    target_score = 10000000
+ 
     finish = False
-
+ 
     while q.not_empty and not finish:
         score, pos, direction, path = q.get()
-        # if pos in memo and memo[pos][0] == direction and memo[pos][1] <= score:
-        #     continue
-        # memo[pos] = (direction, score)
-
+        memo.add((pos, direction))
+        if score > target_score:
+            finish = True
         if pos == end:
-            if not results:
+            if len(results) == 0:
                 target_score = score
-                print(score)
-                results.append(path)
+                print("Part 1: ", score)
+                results.update(path)
             if score == target_score:
-                results.append(path)
-            if score > target_score:
-                finish = True
-
-        path.add(pos)
-
-        q.put((score + 1000, pos, turn_right(direction), path.copy()))
-        q.put((score + 1000, pos, turn_left(direction), path.copy()))
-        forward = (
-            score + 1,
-            (pos[0] + directions[direction][0], pos[1] + directions[direction][1]),
-            direction,
-            path.copy(),
-        )
-        if grid[forward[1]] != "#":
-            q.put(forward)
-    myset = set()
-    for i in results:
-        myset = myset.union(i)
-    for y, line in enumerate(data):
-        for x, char in enumerate(line):
-            if (x, y) in myset:
-                print("O", end="")
-            else:
-                print(char, end="")
-        print()
-    print(len(myset))
+                results.update(path)
+ 
+        for d in [0, -1, 1]:
+            new_dir = (direction + d) % 4
+            dir = directions[new_dir]
+            new_score = score + (abs(d) * 1000) + 1
+            new_pos = (pos[0] + directions[new_dir][0], pos[1] + directions[new_dir][1])
+            new_path = path + [new_pos]
+            if (new_pos, new_dir) in memo:
+                continue
+            if grid[new_pos] != "#":
+                q.put((score + (abs(d) * 1000) + 1, new_pos, new_dir, new_path))
+ 
+    print("Part 2: ", len(results))
