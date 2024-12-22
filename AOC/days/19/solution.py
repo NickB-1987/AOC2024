4b42aa5e36587cf4f38d933e8b6d05b04e5a5583
NickB-1987 <94359987+NickB-1987@users.noreply.github.com>
2024-12-22 10:35:15 +0000

    d19 pt2 done

--- AOC/days/19/solution.py
+++ AOC/days/19/solution.py
@@ -3,7 +3,7 @@ from queue import Queue
 from multiprocessing import Pool
 
 TEST = True
-# TEST = False
+TEST = False
 
 data = open(
             Path(Path(__file__).parent, "test" if TEST else "input"), "r"
@@ -16,34 +16,38 @@ patterns = [line.strip() for line in data[2:]]
 if TEST:
     ...
 
+memo = dict()
+
 def can_be_made(pattern):
     ways = 0
-    memo = set()
+    memo = dict()
     queue = Queue()
     queue.put(pattern)
     while not queue.empty():
         current = queue.get()
+        if current in memo:
+            return memo[current]
         if current == "":
             ways += 1
         for towel in towels:
-            if current.startswith(towel) and current[len(towel):] not in memo:
-                memo.add(current[len(towel):])
+            if current.startswith(towel):
                 queue.put(current[len(towel):])
-    with open(Path(Path(__file__).parent, f"patterns/{pattern}"), "w") as f:
-        f.write(f"{ways}")
+    return ways
+
+def find_pattern(pattern: str):
+    if pattern == "":
+        return 1
+    if pattern in memo:
+        return memo[pattern]
+    ways = 0
+    for towel in towels:
+        if pattern.startswith(towel):
+            ways += find_pattern(pattern[len(towel):])
+    memo[pattern] = ways
     return ways
 
 if __name__ == "__main__":
-    p = Pool()
-    success = 0
-    success2 = 0
-    j = len(patterns)
-    ans = p.map(can_be_made, patterns)
-    # for i, pattern in enumerate(patterns):
-    #     ans = can_be_made(pattern, towels)
-    #     print(f"Pattern {i} / {j}")
-    #     success += bool(ans)
-    #     success2 += ans
+    ans = [find_pattern(p) for p in patterns]
     success = sum([bool(a) for a in ans])
     success2 = sum(ans)
     print("Part1: ", success)
