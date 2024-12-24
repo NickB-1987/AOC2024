1bb3e64a41f7c3ee2c0149f43fc266059b6ac22c
NickB-1987 <94359987+NickB-1987@users.noreply.github.com>
2024-12-24 17:12:16 +0000

    d24 pt1 done

--- AOC/days/24/solution.py
+++ AOC/days/24/solution.py
@@ -1,9 +1,48 @@
 from pathlib import Path
+import operator
 
 TEST = True
-# TEST = False
+TEST = False
 
-data = open(Path(Path(__file__).parent, "test" if TEST else "input"), "r").readlines()
+data = [
+    line.strip()
+    for line in open(
+        Path(Path(__file__).parent, "test2" if TEST else "input"), "r"
+    ).readlines()
+]
+
+known_wires = {}
+gates = []
+
+for line in data:
+    if ":" in line:
+        a, b = line.split(": ")
+        known_wires[a] = b
+    elif line == "":
+        continue
+    else:
+        gate = line.split(" ")
+        if gate[4] not in known_wires:
+            gates.append(gate)
+
+ops = {"AND": operator.and_, "OR": operator.or_, "XOR": operator.xor}
 
 if __name__ == "__main__":
-    ...
+    while gates:
+        for gate in gates:
+            if (
+                gate[0] in known_wires
+                and gate[2] in known_wires
+                and gate[4] not in known_wires
+            ):
+                known_wires[gate[4]] = str(
+                    ops[gate[1]](int(known_wires[gate[0]]), int(known_wires[gate[2]]))
+                )
+                gates.remove(gate)
+    z_s = dict(
+        sorted(
+            {k: v for k, v in known_wires.items() if k.startswith("z")}.items(),
+            reverse=True,
+        )
+    )
+    print("Part 1: ", int("".join(z_s.values()), 2))
